0ef6fec5038fba286cbaf736c3a45eb97feade95
cheng2295 <bestccj@163.com>
2022-09-07 05:12:35 +0800

    add sql

---
+++ user.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS hello;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'ttc');
+INSERT INTO `user` VALUES ('123', 'cj');
+INSERT INTO `user` VALUES ('2', 'wwwt');
+INSERT INTO `user` VALUES ('3', 'ott');
+
+SET FOREIGN_KEY_CHECKS = 1;
